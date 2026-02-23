9b1754a9fe529c31123b7e61ee0be704e805be64
Veerang-112 <dagarveerang@gmail.com>
2026-02-23 15:22:58 +0530

    commit

--- script.js
+++ script.js
@@ -1,44 +1,51 @@
-// // // var num=10
-// // // console.log(num)
-// // // console.log(typeof(num))
+// // // // var num=10
+// // // // console.log(num)
+// // // // console.log(typeof(num))
 
-// // // num=false
-// // // console.log(num)
-// // // console.log(typeof(num))
+// // // // num=false
+// // // // console.log(num)
+// // // // console.log(typeof(num))
 
-// // // num="hello"
-// // // console.log(num)
-// // // console.log(typeof(num))
+// // // // num="hello"
+// // // // console.log(num)
+// // // // console.log(typeof(num))
 
-// // // num=null
-// // // console.log(num)
-// // // console.log(typeof(num))
+// // // // num=null
+// // // // console.log(num)
+// // // // console.log(typeof(num))
 
-// // // num=undefined
-// // // console.log(num)
-// // // console.log(typeof(num))
+// // // // num=undefined
+// // // // console.log(num)
+// // // // console.log(typeof(num))
 
-// // // var h=Symbol(2)
-// // // console.log(h)
-// // // console.log(typeof(h))
-// // // var h1=Symbol(2)
-// // // console.log(h1)
-// // // console.log(typeof(h1))
-// // // if(h==h1){
-// // //     console.log("true")
-// // // }  
-// // // else{
-// // //     console.log("false")
-// // // }
+// // // // var h=Symbol(2)
+// // // // console.log(h)
+// // // // console.log(typeof(h))
+// // // // var h1=Symbol(2)
+// // // // console.log(h1)
+// // // // console.log(typeof(h1))
+// // // // if(h==h1){
+// // // //     console.log("true")
+// // // // }  
+// // // // else{
+// // // //     console.log("false")
+// // // // }
+
+// // var a=10
+// // var b="10"
+// // console.log("<br>")
+// // console.log(a==b) //true
+// // console.log(a===b) //false
 
 // var a=10
-// var b="10"
-// console.log("<br>")
-// console.log(a==b) //true
-// console.log(a===b) //false
+// var b=10
+// if(a == 10 && b == 10){
+//     console.log("true")
+// }
 
-var a=10
-var b=10
-if(a == 10 && b == 10){
-    console.log("true")
+let count;
+console.log("start of loop")
+for(count=0; count<5; count++){
+    console.log(count)
 }
+console.log("end of loop")
