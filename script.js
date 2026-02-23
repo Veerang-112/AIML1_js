d0d02d16fdcc26d82b163fe26ec474358e83a098
Veerang-112 <dagarveerang@gmail.com>
2026-02-23 14:58:21 +0530

    commit

--- script.js
+++ script.js
@@ -1,41 +1,42 @@
-// var num=10
-// console.log(num)
-// console.log(typeof(num))
+// // var num=10
+// // console.log(num)
+// // console.log(typeof(num))
 
-// num=false
-// console.log(num)
-// console.log(typeof(num))
+// // num=false
+// // console.log(num)
+// // console.log(typeof(num))
 
-// num="hello"
-// console.log(num)
-// console.log(typeof(num))
+// // num="hello"
+// // console.log(num)
+// // console.log(typeof(num))
 
-// num=null
-// console.log(num)
-// console.log(typeof(num))
+// // num=null
+// // console.log(num)
+// // console.log(typeof(num))
 
-// num=undefined
-// console.log(num)
-// console.log(typeof(num))
+// // num=undefined
+// // console.log(num)
+// // console.log(typeof(num))
 
-// var h=Symbol(2)
-// console.log(h)
-// console.log(typeof(h))
-// var h1=Symbol(2)
-// console.log(h1)
-// console.log(typeof(h1))
-// if(h==h1){
-//     console.log("true")
-// }  
-// else{
-//     console.log("false")
-// }
+// // var h=Symbol(2)
+// // console.log(h)
+// // console.log(typeof(h))
+// // var h1=Symbol(2)
+// // console.log(h1)
+// // console.log(typeof(h1))
+// // if(h==h1){
+// //     console.log("true")
+// // }  
+// // else{
+// //     console.log("false")
+// // }
 
-var a=10
-var b=20
-var c=30
-var d="<br>"
-document.write("a+b+c=")
-var result=a+b+c
-document.write(result)
-document.write(d)
+// var a=10
+// var b=20
+// var c=30
+// var d="<br>"
+// document.write("a+b+c=")
+// var result=a+b+c
+// document.write(result)
+// document.write(d)
+
