219ff3f8ac405c027fc3674fe6d2b1cda60075b4
Veerang-112 <dagarveerang@gmail.com>
2026-02-27 09:32:39 +0530

    alert function by using button

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
-function add(a,b){
-    return a + b
+function my(){
+    alert("bakwas mt kr")
 }
 
-let f=9
-let g=8
-let sum = add(f,g)
-console.log("The sum of " + f + " and " + g + " is: " + sum)
