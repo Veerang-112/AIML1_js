ee702221e7dc7171f00763316b0a8c96dc6ff9a8
Veerang-112 <dagarveerang@gmail.com>
2026-02-27 10:21:05 +0530

    creating object using object function

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-let person = {
-    name: "John",
-    age: 30,
-    isMarried: false,
-    address: {
-        street: "123 Main St",
-        city: "New York",}
-};
-console.log(person.address.street)
+let person=Object()
+person.name="John";
+person.age=30;
+person.city="New York";
+console.log(person);
